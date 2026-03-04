ae3cb4d4262622eeac52c3ee225df6977b283cc9
shivrajkulkarni53 <chmcs2026.shivraj.kulkarni@gmail.com>
2026-03-04 20:45:27 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // your backend URL
+  //baseURL: "http://localhost:3000",// your backend URL
+  baseURL: "https://movie-library-me8c.onrender.com"
   headers: {
     "Content-Type": "application/json"
   }
